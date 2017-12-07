eabde488d402ebba80f1fc0580b3b3e6e35c7633
kipphr <kipphr@rose-hulman.edu>
2017-12-06 20:54:23 -0500

    progress p0.

--- src/problem0.py
+++ src/problem0.py
@@ -8,8 +8,8 @@ These problems illustrate concepts that previous problems have not emphasized:
   -- animation (Problem 0c)
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Dutch Kipp.
+"""  # done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -146,7 +146,7 @@ def problem0a(n):
            since (2 + 4 + 6) is 12, which is NOT odd.
     """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # done: 2. Implement and test this function.
     #          Tests have been written for you (above).
     #
     ####################################################################
@@ -163,6 +163,11 @@ def problem0a(n):
     #        Simply try a few examples to convince yourself of this.
     #        ASK FOR HELP if you do not understand this hint.
     # ------------------------------------------------------------------
+
+    if (sum_of_digits(n) % 2) == 1:
+        return True
+
+    return False
 
 
 def run_test_problem0b():
@@ -227,6 +232,13 @@ def problem0b(n):
     #    **  use (call) the   is_prime   function that is DEFINED ABOVE.
     ####################################################################
     # ------------------------------------------------------------------
+
+    count = 0
+    for k in range(2, n+1):
+        if is_prime(k) == True:
+            count = count + 1
+
+    return count
 
 
 def run_test_problem0c():
